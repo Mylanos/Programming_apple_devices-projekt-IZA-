3e6e491dea821b1650fbadc6f960365a75980c1f
Marek Žiška <marek@ziska.sk>
2020-05-19 00:51:15 +0200

    added progress bar

---
+++ Tabater/Views/ProgressBarView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressBarView.swift
+//  Tabater
+//
+//  Created by Marek Žiška on 19/05/2020.
+//  Copyright © 2020 Marek Žiška. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProgressBarView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct ProgressBarView_Previews: PreviewProvider {
+    static var previews: some View {
+        ProgressBarView()
+    }
+}
